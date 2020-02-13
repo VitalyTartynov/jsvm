fb2966003f49c93014a05d0c1b88dd26f81f8b56
Vitaly.Tartynov <Vitaly.Tartynov@GMail.com>
2020-02-13 13:11:17 +0200

    Added eslint.

--- src/sum.js
+++ src/sum.js
@@ -1,5 +1,5 @@
 ﻿function sum(a, b) {
-    return a + b;
+  return a + b;
 }
 
 module.exports = sum;
--- src/sum.test.js
+++ src/sum.test.js
@@ -1,5 +1,5 @@
 ﻿const sum = require('./sum');
 
 test('sum 1 and 2 equal 3', () => {
-    expect(sum(1, 2)).toBe(3);
+  expect(sum(1, 2)).toBe(3);
 });
