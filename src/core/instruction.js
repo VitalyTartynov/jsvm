14388629d8bf3e24ac9eb5c45b8ac889a268c1c1
Vitaly.Tartynov <Vitaly.Tartynov@GMail.com>
2020-02-23 17:44:45 +0200

    Added some jsdoc information.

--- src/core/instruction.js
+++ src/core/instruction.js
@@ -1,4 +1,10 @@
 ﻿class Instruction {
+    /**
+     * CPU instruction instance
+     * @param opcode Operation code.
+     * @param command Assemble command.
+     * @param description Some description about instruction and operands.
+     */
     constructor(opcode, command, description) {
         this.command = command;
         this.opcode = opcode;
--- src/core/register.js
+++ src/core/register.js
@@ -1,4 +1,11 @@
 ﻿class Register {
+
+    /**
+     * Register instance
+     * @param name Human-friendly name of register.
+     * @param address Address of register in internal register memory.
+     * @param description Some description about register.
+     */
     constructor(name, address, description) {
         this.name = name;
         this.address = address;
