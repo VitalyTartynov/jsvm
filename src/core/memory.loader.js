cbdbe821e57fc857bb4c4c9ecddd07c185b43308
Vitaly.Tartynov <Vitaly.Tartynov@GMail.com>
2020-02-24 10:27:14 +0200

    Added memory loader.

---
+++ src/core/memory.loader.js
@@ -0,0 +1,29 @@
+﻿const format = require('./format');
+
+class MemoryLoader {
+    save(memory) {
+        let result = '';
+        for (let i = 0; i < memory.length; i++) {
+            result += format.asByte(memory.byteAt[i]);
+            if (i !== memory.length - 1) {
+                result += ' ';
+            }
+        }
+        
+        return result;
+    }
+    
+    load(data, memory) {
+        const values = data.split(' ');
+        
+        if (values.length > memory.length) {
+            throw new Error('Program is bigger than memory');
+        }
+        
+        for (let i = 0; i < memory.length; i++) {
+            memory.byteAt[i] = values[i];
+        }       
+    }
+}
+
+module.exports = MemoryLoader;
