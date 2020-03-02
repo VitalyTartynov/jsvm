a6b967f870c11b8ea0b455fe9d6f9d16eaac8e42
Vitaly.Tartynov <Vitaly.Tartynov@GMail.com>
2020-03-02 11:59:05 +0200

    Added architecture set. Moved registers to sample architecture.

--- src/architecture/sample/register.constant.js
+++ src/architecture/sample/register.constant.js
@@ -1,4 +1,4 @@
-﻿const Register = require('./register');
+﻿﻿const Register = require('../../core/register');
 
 const IP = new Register('IP', 0x00, 'Instruction Pointer');
 const AC = new Register('AC', 0x02, 'Accumulator');
@@ -8,6 +8,8 @@ const R2 = new Register('R2', 0x08, 'General Purpose register 2');
 const R3 = new Register('R3', 0x0A, 'General Purpose register 3');
 const R4 = new Register('R4', 0x0C, 'General Purpose register 4');
 
+const ALL = [IP, AC, SP, R1, R2, R3, R4];
+
 module.exports = {
   IP,
   AC,
@@ -16,4 +18,5 @@ module.exports = {
   R2,
   R3,
   R4,
+  ALL
 };
