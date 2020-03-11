307ac8a03e058e4f187c73884261831889d864e8
vitaly.tartynov <Vitaly.Tartynov@GMail.com>
2020-03-11 21:07:21 +0200

    Added browserify.

---
+++ src/vm/virtual-machine.js
@@ -0,0 +1,21 @@
+﻿const Cpu = require('./cpu');
+const Alu = require('./alu');
+const Memory = require('./memory');
+const Registers = require('./registers');
+
+const REGISTER = require('../architecture/sample/register.constant');
+
+/**
+ * Virtual machine.
+ */
+class VirtualMachine {
+  constructor(ramSize = 64) {
+    this.ram = new Memory(ramSize);
+    this.registers = new Registers(REGISTER.ALL);
+    this.alu = new Alu(this.ram, this.registers);
+    this.flash = new Memory(0);
+    this.cpu = new Cpu(this.ram, this.registers, this.alu, this.flash);
+  }
+}
+
+module.exports = VirtualMachine;
---
+++ src/vm/virtual-machine.test.js
@@ -0,0 +1,13 @@
+﻿const VirtualMachine = require('./virtual-machine');
+
+describe('VirtualMachine', () => {
+  let virtualMachine;
+  
+  beforeEach(() => {
+    virtualMachine = new VirtualMachine();
+  });
+  
+  test('should be created', () => {
+    expect(virtualMachine).toBeTruthy();
+  });
+});
