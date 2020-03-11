417fbcc3263e1295394da2dede0fb7db42bdb8b0
Vitaly Tartynov <Vitaly.Tartynov@GMail.com>
2020-03-11 17:03:43 +0200

    Added text screen and tests for it. (#36)

---
+++ src/vm/text-screen.js
@@ -0,0 +1,17 @@
+﻿/**
+ * Text screen device.
+ */
+class TextScreen {
+  constructor() {
+  }
+  
+  getUint8() { }
+  setUint8() { }
+  getUint16() { }
+  setUint16(address, value) {
+    const character = String.fromCharCode(value);
+    console.log(character);
+  }
+}
+
+module.exports = TextScreen;
---
+++ src/vm/text-screen.test.js
@@ -0,0 +1,28 @@
+﻿const MemoryMapper = require('./memory-mapper');
+const Memory = require('./memory');
+const TextScreen = require('./text-screen');
+
+describe('TextScreen', () => {
+  const ramSize = 64;
+  let memoryMapper;
+  let ram;
+  let textScreen;
+  
+  beforeEach(() => {
+    ram = new Memory(ramSize);
+    memoryMapper = new MemoryMapper();
+    textScreen = new TextScreen();
+    
+    memoryMapper.map(ram, 0, ramSize);
+    memoryMapper.map(textScreen, 0x0010, 0x0020, true);
+  });
+  
+  test('should be created', () => {
+    expect(textScreen).toBeTruthy();
+  });
+  
+  test('should log characters via text screen', () => {
+    memoryMapper.setUint16(0x0010, 'G'.charCodeAt(0));
+    memoryMapper.setUint16(0x0010, 'O'.charCodeAt(0));
+  });
+});
